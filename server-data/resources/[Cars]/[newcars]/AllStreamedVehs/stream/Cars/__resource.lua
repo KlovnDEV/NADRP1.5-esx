c6be6e130d6d3e9278474606a2e2ba82af8ce41d
Tate <94901862+Tate8383@users.noreply.github.com>
2022-09-07 21:59:11 -0700

    new car pack
    cars removed over polly this is the last one

---
+++ server-data/resources/[Cars]/[newcars]/AllStreamedVehs/stream/Cars/__resource.lua
@@ -0,0 +1,469 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+    'PiikesVIP1-vehicles.meta',
+    'PiikesVIP1-carvariations.meta',
+    'PiikesVIP1-carcols.meta',
+    'PiikesVIP1-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP1-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP1-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP1-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP1-carvariations.meta'
+
+files {
+    'PiikesVIP2-vehicles.meta',
+    'PiikesVIP2-carvariations.meta',
+    'PiikesVIP2-carcols.meta',
+    'PiikesVIP2-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP2-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP2-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP2-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP2-carvariations.meta'
+
+files {
+    'PiikesVIP3-vehicles.meta',
+    'PiikesVIP3-carvariations.meta',
+    'PiikesVIP3-carcols.meta',
+    'PiikesVIP3-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP3-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP3-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP3-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP3-carvariations.meta'
+
+files {
+    'PiikesVIP4-vehicles.meta',
+    'PiikesVIP4-carvariations.meta',
+    'PiikesVIP4-carcols.meta',
+    'PiikesVIP4-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP4-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP4-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP4-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP4-carvariations.meta'
+
+files {
+    'PiikesVIP5-vehicles.meta',
+    'PiikesVIP5-carvariations.meta',
+    'PiikesVIP5-carcols.meta',
+    'PiikesVIP5-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP5-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP5-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP5-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP5-carvariations.meta'
+
+files {
+    'PiikesVIP6-vehicles.meta',
+    'PiikesVIP6-carvariations.meta',
+    'PiikesVIP6-carcols.meta',
+    'PiikesVIP6-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP6-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP6-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP6-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP6-carvariations.meta'
+
+files {
+    'PiikesVIP7-vehicles.meta',
+    'PiikesVIP7-carvariations.meta',
+    'PiikesVIP7-carcols.meta',
+    'PiikesVIP7-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP7-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP7-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP7-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP7-carvariations.meta'
+
+files {
+    'PiikesVIP8-vehicles.meta',
+    'PiikesVIP8-carvariations.meta',
+    'PiikesVIP8-carcols.meta',
+    'PiikesVIP8-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP8-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP8-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP8-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP8-carvariations.meta'
+
+files {
+    'PiikesVIP9-vehicles.meta',
+    'PiikesVIP9-carvariations.meta',
+    'PiikesVIP9-carcols.meta',
+    'PiikesVIP9-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP9-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP9-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP9-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP9-carvariations.meta'
+
+files {
+    'PiikesVIP10-vehicles.meta',
+    'PiikesVIP10-carvariations.meta',
+    'PiikesVIP10-carcols.meta',
+    'PiikesVIP10-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP10-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP10-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP10-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP10-carvariations.meta'
+
+files {
+    'PiikesVIP11-vehicles.meta',
+    'PiikesVIP11-carvariations.meta',
+    'PiikesVIP11-carcols.meta',
+    'PiikesVIP11-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP11-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP11-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP11-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP11-carvariations.meta'
+
+files {
+    'PiikesVIP12-vehicles.meta',
+    'PiikesVIP12-carvariations.meta',
+    'PiikesVIP12-carcols.meta',
+    'PiikesVIP12-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP12-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP12-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP12-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP12-carvariations.meta'
+
+files {
+    'PiikesVIP13-vehicles.meta',
+    'PiikesVIP13-carvariations.meta',
+    'PiikesVIP13-carcols.meta',
+    'PiikesVIP13-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP13-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP13-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP13-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP13-carvariations.meta'
+
+files {
+    'PiikesVIP14-vehicles.meta',
+    'PiikesVIP14-carvariations.meta',
+    'PiikesVIP14-carcols.meta',
+    'PiikesVIP14-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP14-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP14-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP14-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP14-carvariations.meta'
+
+files {
+    'PiikesVIP15-vehicles.meta',
+    'PiikesVIP15-carvariations.meta',
+    'PiikesVIP15-carcols.meta',
+    'PiikesVIP15-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP15-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP15-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP15-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP15-carvariations.meta'
+
+files {
+    'PiikesVIP16-vehicles.meta',
+    'PiikesVIP16-carvariations.meta',
+    'PiikesVIP16-carcols.meta',
+    'PiikesVIP16-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP16-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP16-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP16-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP16-carvariations.meta'
+
+files {
+    'PiikesVIP17-vehicles.meta',
+    'PiikesVIP17-carvariations.meta',
+    'PiikesVIP17-carcols.meta',
+    'PiikesVIP17-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP17-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP17-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP17-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP17-carvariations.meta'
+
+files {
+    'PiikesVIP18-vehicles.meta',
+    'PiikesVIP18-carvariations.meta',
+    'PiikesVIP18-carcols.meta',
+    'PiikesVIP18-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP18-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP18-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP18-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP18-carvariations.meta'
+
+files {
+    'PiikesVIP19-vehicles.meta',
+    'PiikesVIP19-carvariations.meta',
+    'PiikesVIP19-carcols.meta',
+    'PiikesVIP19-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP19-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP19-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP19-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP19-carvariations.meta'
+
+files {
+    'PiikesVIP20-vehicles.meta',
+    'PiikesVIP20-carvariations.meta',
+    'PiikesVIP20-carcols.meta',
+    'PiikesVIP20-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP20-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP20-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP20-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP20-carvariations.meta'
+
+files {
+    'PiikesVIP21-vehicles.meta',
+    'PiikesVIP21-carvariations.meta',
+    'PiikesVIP21-carcols.meta',
+    'PiikesVIP21-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP21-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP21-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP21-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP21-carvariations.meta'
+
+files {
+    'PiikesVIP22-vehicles.meta',
+    'PiikesVIP22-carvariations.meta',
+    'PiikesVIP22-carcols.meta',
+    'PiikesVIP22-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP22-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP22-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP22-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP22-carvariations.meta'
+
+files {
+    'PiikesVIP23-vehicles.meta',
+    'PiikesVIP23-carvariations.meta',
+    'PiikesVIP23-carcols.meta',
+    'PiikesVIP23-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP23-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP23-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP23-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP23-carvariations.meta'
+
+files {
+    'PiikesVIP24-vehicles.meta',
+    'PiikesVIP24-carvariations.meta',
+    'PiikesVIP24-carcols.meta',
+    'PiikesVIP24-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP24-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP24-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP24-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP24-carvariations.meta'
+
+files {
+    'PiikesVIP25-vehicles.meta',
+    'PiikesVIP25-carvariations.meta',
+    'PiikesVIP25-carcols.meta',
+    'PiikesVIP25-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP25-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP25-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP25-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP25-carvariations.meta'
+
+files {
+    'PiikesVIP26-vehicles.meta',
+    'PiikesVIP26-carvariations.meta',
+    'PiikesVIP26-carcols.meta',
+    'PiikesVIP26-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP26-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP26-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP26-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP26-carvariations.meta'
+
+files {
+    'PiikesVIP27-vehicles.meta',
+    'PiikesVIP27-carvariations.meta',
+    'PiikesVIP27-carcols.meta',
+    'PiikesVIP27-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP27-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP27-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP27-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP27-carvariations.meta'
+
+files {
+    'PiikesVIP28-vehicles.meta',
+    'PiikesVIP28-carvariations.meta',
+    'PiikesVIP28-carcols.meta',
+    'PiikesVIP28-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP28-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP28-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP28-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP28-carvariations.meta'
+
+files {
+    'PiikesVIP29-vehicles.meta',
+    'PiikesVIP29-carvariations.meta',
+    'PiikesVIP29-carcols.meta',
+    'PiikesVIP29-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP29-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP29-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP29-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP29-carvariations.meta'
+
+files {
+    'PiikesVIP30-vehicles.meta',
+    'PiikesVIP30-carvariations.meta',
+    'PiikesVIP30-carcols.meta',
+    'PiikesVIP30-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP30-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP30-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP30-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP30-carvariations.meta'
+
+files {
+    'PiikesVIP31-vehicles.meta',
+    'PiikesVIP31-carvariations.meta',
+    'PiikesVIP31-carcols.meta',
+    'PiikesVIP31-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP31-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP31-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP31-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP31-carvariations.meta'
+
+files {
+    'PiikesVIP32-vehicles.meta',
+    'PiikesVIP32-carvariations.meta',
+    'PiikesVIP32-carcols.meta',
+    'PiikesVIP32-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP32-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP32-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP32-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP32-carvariations.meta'
+
+files {
+    'PiikesVIP33-vehicles.meta',
+    'PiikesVIP33-carvariations.meta',
+    'PiikesVIP33-carcols.meta',
+    'PiikesVIP33-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP33-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP33-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP33-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP33-carvariations.meta'
+
+files {
+    'PiikesVIP34-vehicles.meta',
+    'PiikesVIP34-carvariations.meta',
+    'PiikesVIP34-carcols.meta',
+    'PiikesVIP34-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP34-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP34-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP34-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP34-carvariations.meta'
+
+files {
+    'PiikesVIP35-vehicles.meta',
+    'PiikesVIP35-carvariations.meta',
+    'PiikesVIP35-carcols.meta',
+    'PiikesVIP35-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP35-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP35-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP35-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP35-carvariations.meta'
+
+files {
+    'PiikesVIP36-vehicles.meta',
+    'PiikesVIP36-carvariations.meta',
+    'PiikesVIP36-carcols.meta',
+    'PiikesVIP36-handling.meta',    	
+	
+}
+ 
+data_file 'HANDLING_FILE' 'PiikesVIP36-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'PiikesVIP36-vehicles.meta'
+data_file 'CARCOLS_FILE' 'PiikesVIP36-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'PiikesVIP36-carvariations.meta'
